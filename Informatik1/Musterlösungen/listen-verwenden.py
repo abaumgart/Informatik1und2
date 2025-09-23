6c0c607eb82236f6a75022f00b4bbd7b27a5ce9f
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2025-09-23 13:45:16 +0200

    update

--- Informatik1/Musterlösungen/listen-verwenden.py
+++ Informatik1/Musterlösungen/listen-verwenden.py
@@ -1,42 +1,20 @@
-eineListe=[2,5, 10,7]
-summe=0
-anzahlElemente =len(eineListe)
+import numpy as np
 
-print(eineListe)
-eineListe.sort()
-print(eineListe)
-eineListe.reverse()
-print(eineListe)
-#Wert anhängen
-eineListe.append(45)
-print(eineListe)
-#Wert löschen
-eineListe.remove(5)
-print(eineListe)
-#Wert an bestimmer Position einfügen
-eineListe.insert(2,111)
-print(eineListe)
-#Wert an bestimmer Position löschen
-del eineListe[2]
-print(eineListe)
-#Maximalwert bestimmen
-print(max(eineListe))
-#Minimalwert bestimmen
-print(min(eineListe))
-#Summe der werte einer Liste bestimmen
-print(sum(eineListe))
-#for i in range(0,anzahlElemente,1):
- #   summe=eineListe[i]+summe
-#print (summe/anzahlElemente)
- 
- # 3x3 Matrix
- 
-meineMatrix=[
-                 [1,2,3],#erste Zeile
-                 [4,5,6],#zweite Zeile
-                 [7,8,9],#dritte Zeile
-                ]
- #Zugriff auf das Element in der zweiten Zeile,
- #dritten Spalte
-element=meineMatrix[1][2]
-print(element)
+A=np.matrix([[1,2],
+             [3,4]
+            ])
+B=np.matrix([[5,6],
+             [7,8]
+            ])
+
+C=A*B
+print(C)
+
+y=np.poly1d([-10,0,0,0,-1,0,1,-8])
+
+y1=y.deriv(1)
+print (y1)
+
+
+
+
