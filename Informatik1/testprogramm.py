52c1c6e1d3cb5e2ab55665c0e17f4fbfe6528810
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2026-04-15 08:40:45 +0200

    update

--- Informatik1/testprogramm.py
+++ Informatik1/testprogramm.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 x = sp.symbols('x')
 
 # Funktion mit SymPy definieren
-funktion = x**2
+funktion = x**5-3*x**4+2*x**3-x+1
 
 # Ableitung berechnen
 ableitung = sp.diff(funktion, x)
@@ -14,9 +14,11 @@ ableitung = sp.diff(funktion, x)
 print("Funktion:", funktion)
 print("Ableitung:", ableitung)
 
+f=sp.lambdify(x, funktion, "numpy")
+
 # Werte mit NumPy erzeugen
 x_werte = np.linspace(-5, 5, 100)
-y_werte = x_werte**2
+y_werte = f(x_werte)
 
 # Grafik mit Matplotlib zeichnen
 plt.plot(x_werte, y_werte)
